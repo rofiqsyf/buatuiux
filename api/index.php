027bc6ceb01bad7e05c78816b4b49db15ee3bbb8
rofiqsyf <130855880+rofiqsyf@users.noreply.github.com>
2026-08-12 13:28:32 +0700

    fix: route laravel cache to tmp to prevent read-only filesystem crash

--- api/index.php
+++ api/index.php
@@ -1,12 +1,4 @@
 <?php
-echo "HELLO FROM VERCEL PHP!";
-exit;
-error_reporting(E_ALL);
-
-/**
- * Entry point for Vercel Serverless Functions
- * Forward requests to Laravel's normal entry point
- */
 $envVars = [
     'APP_ENV' => 'production',
     'APP_DEBUG' => 'true',
@@ -14,7 +6,12 @@ $envVars = [
     'CACHE_DRIVER' => 'array',
     'LOG_CHANNEL' => 'stderr',
     'SESSION_DRIVER' => 'cookie',
-    'VIEW_COMPILED_PATH' => '/tmp'
+    'VIEW_COMPILED_PATH' => '/tmp',
+    'APP_CONFIG_CACHE' => '/tmp/config.php',
+    'APP_SERVICES_CACHE' => '/tmp/services.php',
+    'APP_PACKAGES_CACHE' => '/tmp/packages.php',
+    'APP_ROUTES_CACHE' => '/tmp/routes.php',
+    'APP_EVENTS_CACHE' => '/tmp/events.php',
 ];
 foreach ($envVars as $key => $value) {
     putenv("$key=$value");
