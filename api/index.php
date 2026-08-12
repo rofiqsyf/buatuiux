b18296160960613bb164cc51282c88096db9bde0
rofiqsyf <130855880+rofiqsyf@users.noreply.github.com>
2026-08-12 11:43:18 +0700

    fix: inject env vars directly and track sqlite db

--- api/index.php
+++ api/index.php
@@ -4,4 +4,12 @@
  * Entry point for Vercel Serverless Functions
  * Forward requests to Laravel's normal entry point
  */
+putenv('APP_ENV=production');
+putenv('APP_DEBUG=true');
+putenv('APP_KEY=base64:9FpY0bN8dOa4M2pY0bN8dOa4M2pY0bN8dOa4M2pY0bM=');
+putenv('CACHE_DRIVER=array');
+putenv('LOG_CHANNEL=stderr');
+putenv('SESSION_DRIVER=cookie');
+putenv('VIEW_COMPILED_PATH=/tmp');
+
 require __DIR__ . '/../public/index.php';
